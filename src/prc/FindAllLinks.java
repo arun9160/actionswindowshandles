759dd40bad00547a2a56c763379ffa5d5a301f2e
Siva <Siva@DESKTOP-B8IRFS0>
2020-07-07 08:32:38 +0530

    Find all links using list

---
+++ src/prc/FindAllLinks.java
@@ -0,0 +1,29 @@
+package prc;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindAllLinks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\WorkSpace\\SeleniumDemos\\Libs\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+		for(int i = 0; i<=links.size();i++) {
+			
+			System.out.println(links.get(i).getAttribute("href"));
+		}
+	}	
+
+}
